7b8204520ba4853b2644fab3d66c7dc191b31060
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-26 06:41:18 -0400

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -127,7 +127,7 @@ class MainMenuState extends MusicBeatState
 
 	function createMenuItem(name:String, x:Float, y:Float):FlxSprite
 	{
-		if (optionShit[i] == 'nothing') {continue;}
+		if (optionShit[i] == 'nothing') {}
 		var menuItem:FlxSprite = new FlxSprite(x, y).loadGraphic(Paths.image('mainmenu/$name'));
 		menuItem.updateHitbox();
 	       var scr:Float = (optionShit.length - 4) * 0.135;
