ee425d07e35a47649a85af157f7859d5f4bac6dc
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-26 06:44:50 -0400

    Update PlayMenuState.hx

--- source/PlayMenuState.hx
+++ source/PlayMenuState.hx
@@ -121,7 +121,7 @@ class PlayMenuState extends MusicBeatState
 	function createMenuItem(name:String, x:Float, y:Float):FlxSprite
 	{
 		if (optionShit[i] == 'nothing') {}
-		var menuItem:FlxSprite = new FlxSprite(x, y).loadGraphic(Paths.image('mainmenu/$name'));
+		var menuItem:FlxSprite = new FlxSprite(x, y).loadGraphic(Paths.image('mainmenu/' + optionShit[i]));
 		menuItem.updateHitbox();
 	       var scr:Float = (optionShit.length - 4) * 0.135;
 	       if (optionShit.length < 6)
